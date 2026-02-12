75d2dcac0ebb8f9bcf4098f0292e5c1792c8aff7
Erickson <rixon.code@gmail.com>
2026-02-12 22:48:25 +0800

    testing only

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,10 @@ import express from 'express';
 
 const app = express();
 
-//sending a request in the server
 app.get('/', (req, res) => {
 	res.send('Hello World');
 })
 
-//setting up a listening port, this is only static, we can set this up dynamically later using .env dependencies
 app.listen(3000, () => {
 	console.log('I am listening in port 3000')
 })
